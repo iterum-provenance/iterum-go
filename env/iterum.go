dfe0b1217504c6260f0de5eda8f2ddc64926b4ce
Mantsje <edserapperloo@hotmail.com>
2020-04-28 11:41:25 +0200

    add PipelineHash to iterum env variables

--- env/iterum.go
+++ env/iterum.go
@@ -5,6 +5,7 @@ import "os"
 const (
 	dataVolumePathEnv = "DATA_VOLUME_PATH"
 	nameEnv           = "ITERUM_NAME"
+	pipelineHashEnv   = "PIPELINE_HASH"
 )
 
 // DataVolumePath is the path to the shared volume within this pod
@@ -13,6 +14,9 @@ var DataVolumePath = os.Getenv(dataVolumePathEnv)
 // ProcessName is the name (user defined) for this transformation step/fragmenter/etc
 var ProcessName = os.Getenv(nameEnv)
 
+// PipelineHash is th hash associated with this pipeline run
+var PipelineHash = os.Getenv(pipelineHashEnv)
+
 // VerifyIterumEnvs checks whether each of the environment variables returned a non-empty value
 func VerifyIterumEnvs() error {
 	if DataVolumePath == "" {
@@ -20,6 +24,9 @@ func VerifyIterumEnvs() error {
 	}
 	if ProcessName == "" {
 		return ErrEnvironment(nameEnv, ProcessName)
+	}
+	if PipelineHash == "" {
+		return ErrEnvironment(pipelineHashEnv, PipelineHash)
 	}
 	return nil
 }
